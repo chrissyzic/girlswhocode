1f2dbf6051349559753070a909a926ec29cfaa0d
Chrissy Z <chrissy.ziccarelli@gmail.com>
2015-07-05 17:02:00 -0500

    Update W2_Calcultor.py

--- W2_Calcultor.py
+++ W2_Calcultor.py
@@ -1,6 +1,10 @@
 def addNumbers(first_number, second_number):
-  for i in range(second_number):
-    first_number = first_number + 1
-  print first_number
+    if second_number < 0:
+        for i in range(abs(second_number)):
+            first_number = first_number - 1
+    else:
+        for i in range(second_number):
+            first_number = first_number + 1
+    print first_number
   
-addNumbers(5,6)
+addNumbers(-5,-10)
