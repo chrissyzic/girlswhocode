2c9b73c983aa2102a9240ab81c37f3775f3cc836
Chrissy Z <chrissy.ziccarelli@gmail.com>
2015-07-20 17:04:52 -0500

    Create W3_FishTankOne.js

---
+++ W3_FishTankOne.js
@@ -0,0 +1,14 @@
+background(214, 237, 255);
+noStroke();
+var i = 0;
+
+for(i = 0; i < random(3,6); i++){
+    fill(0, random(100,250), random(0,255));
+    //ellipse(random(10,360),random(15,355), random(30,40), random(30,40));
+    var x = random(10,260);
+    var y = random(35,300);
+    var cx1 = random(25,300);
+    
+    bezier(x, y, cx1, y+150, cx1, y-100, x, y+40);
+
+}
