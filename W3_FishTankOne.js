e3f86354fc2111202d3ea099bb06e9fa0df86ad4
Chrissy Z <chrissy.ziccarelli@gmail.com>
2015-07-20 17:05:25 -0500

    Update W3_FishTankOne.js

--- W3_FishTankOne.js
+++ W3_FishTankOne.js
@@ -2,7 +2,7 @@ background(214, 237, 255);
 noStroke();
 var i = 0;
 
-for(i = 0; i < random(3,6); i++){
+for(i = 0; i < random(3,11); i++){
     fill(0, random(100,250), random(0,255));
     //ellipse(random(10,360),random(15,355), random(30,40), random(30,40));
     var x = random(10,260);
