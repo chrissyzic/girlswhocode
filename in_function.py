ff11b8e241c6be714327dac869df96bda026c511
Chrissy Z <chrissy.ziccarelli@gmail.com>
2016-02-26 10:52:18 -0500

    Create in_function.py

---
+++ in_function.py
@@ -0,0 +1,13 @@
+#Take a look at the code below, what will the last two statements return and WHY? 
+#Note: this is not a pseudocode, it is actually Python. 
+#Even if you know Python try to explain why you answered the way you did based on context clues in the code presented.
+
+day = "Friday"
+week = ["Monday", "Tuesday", "Wednesday", "Thursday", Friday"]
+
+print "Friday" in day #will return True
+print "Friday" in week #will return True
+print "Fri" in day #will return True
+print "Fri" in week #will return False
+print "day" in day
+print "day" in week
